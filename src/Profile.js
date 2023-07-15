ac6a00204c80b89d622c5758a99bd39eb9242f77
Aykhan <ayxanx17@gmail.com>
2023-07-15 11:34:06 +0400

    second commit

--- src/Profile.js
+++ src/Profile.js
@@ -1,37 +1,46 @@
-import React from 'react'
+import React from "react";
 
-const avatar = "https://www.newvisiontheatres.com/wp-content/uploads/2023/06/Dwayne-Johnson.jpg";
+const avatar =
+  "https://www.newvisiontheatres.com/wp-content/uploads/2023/06/Dwayne-Johnson.jpg";
 const name = "Rock";
 const date = new Date(2020, 10, 10);
 
-const subStyle={
-    marginTop: "-9%",
-    textAlign: "center",
-    color: "black",
+const subStyle = {
+  textAlign: "center",
+  color: "white",
+  backgroundColor: "black",
 };
 export default function Profile() {
   return (
     <div>
-      <section style={{
-        width: "250px",
-        height: "250px",
-        margin: "auto",
-        border: "5px solid springgreen",
-        marginTop: "100px",
-        borderRadius: "50%",
-        padding: "5%",
-        boxSizing: 'border-box'
-      }}>
-
-        <img src={avatar} alt='Avatar' style={{
-            width: "150px",
-            height: "150px",
+      <h3 style={subStyle}>- - -</h3>
+      <h3 style={subStyle}>- - -</h3>
+      <section
+        style={{
+          width: "250px",
+          height: "250px",
+          margin: "auto",
+          border: "5px solid springgreen",
+          borderRadius: "50%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <img
+          src={avatar}
+          alt="Avatar"
+          style={{
+            width: "90%",
+            height: "90%",
             margin: "auto",
-            borderRadius: "50%"
-        }}></img>
-        <h3 style={subStyle}>{name}</h3>
-        <h3 style={subStyle}>{date.getFullYear()}</h3>
+            borderRadius: "50%",
+          }}
+        ></img>
       </section>
+
+      <h3 style={subStyle}>{name}</h3>
+      <h3 style={subStyle}>{date.getFullYear()}</h3>
     </div>
-  )
+  );
 }
